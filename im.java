a5b05530350078e50bc689845f77dd5c0ad18ea2
DUNTZI <124770514+DUNTZI@users.noreply.github.com>
2025-05-27 00:36:53 +0800

    TEST

--- im.java
+++ im.java
@@ -22,13 +22,13 @@ public class im extends JPanel {
 
         // ===== CONTENT PANEL (Switches Content in Center) =====
         contentPanel = new JPanel(new BorderLayout());
-        JLabel defaultLabel = new JLabel("WELCOME FINANCE MANAGER", SwingConstants.CENTER);
+        JLabel defaultLabel = new JLabel("WELCOME INVENTORY MANAGER", SwingConstants.CENTER);
         defaultLabel.setFont(new Font("Arial", Font.BOLD, 20));
         contentPanel.add(defaultLabel, BorderLayout.CENTER);
 
         // ===== BOTTOM BUTTON PANEL =====
         JPanel bottomPanel = new JPanel(new GridLayout(1, 7)); // 6 buttons + 1 Exit button
-        String[] buttonNames = {"Item List", "PO List", "Inventory","Inventory Report"};
+        String[] buttonNames = {"Item List", "PO List", "Inventory", "Inventory Report"};
         String[] classNames = {"item_v", "po_v", "inventory_e", "inventory_r"};
 
         for (int i = 0; i < buttonNames.length; i++) {
